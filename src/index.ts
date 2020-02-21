44e86f612ec5527e1648072fcf48ca89916abea5
hsz1273327 <hsz1273327@gmail.com>
2020-02-21 14:56:30 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import * as Pino from 'pino'
+
+const logger = Pino({name:"myapp"})
+
 const promises:Array<Promise<number>> = [
     new Promise<number>((resolve:(value:number)=>void,reject) => resolve(1)),
     new Promise<number>((resolve:(value:number)=>void,reject) => resolve(2)),
@@ -6,7 +10,7 @@ const promises:Array<Promise<number>> = [
 
 async function test(hello: string):Promise<void> {
     for await (const p of promises) {
-        console.log(`${hello},${p}`)
+        logger.info(`${hello},${p}`)
     }
 }
 test("hello")

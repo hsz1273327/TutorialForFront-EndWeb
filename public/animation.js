32244a4abfb3aa70bcfed8824d8e44aaee483bc8
hsz1273327 <hsz1273327@gmail.com>
2020-03-10 00:12:39 +0800

    init

---
+++ public/animation.js
@@ -0,0 +1,16 @@
+function animation(point,target){
+    point.click(function(){
+        var i = 0
+        while (i<5){
+            target.css("color","red")
+                .fadeTo("slow",0.15)
+                .delay(500)
+                .fadeTo("slow",0.85)
+                .delay(500)
+            i+=1
+        }
+    })
+}
+var point = $("#img")
+var target = $("#img")
+animation(point,target)

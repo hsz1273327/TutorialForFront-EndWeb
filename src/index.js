0eb24af4c6f455a763d0b966b725cfd0f4de46db
hsz1273327 <hsz1273327@gmail.com>
2020-03-10 21:28:24 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,32 +1,23 @@
-function _render_template() {
-    let template = document.querySelector('#test_template')
-    let content = document.querySelector('#content').value
-    let placehold = template.content.querySelector("#content_placehold")
-    placehold.innerText = content
-    let now = new Date().toString()
-    template.content.querySelector("time").innerText = now
-    let rendered_content = document.importNode(template.content, true)
-    return rendered_content
-}
 
-function _render_dom(rendered_content) {
-    let root = document.querySelector('#root')
-    let form = document.querySelector('#form')
-    root.insertBefore(rendered_content, form)
-}
-
-function render() {
-    if ('content' in document.createElement('template')) {
-        let rendered_content = _render_template()
-        _render_dom(rendered_content)
-    } else {
-        console.log("浏览器不支持模板")
+let ImgRender = {
+    status: false,
+    render: function (){
+        let image = document.createElement("img")
+        if (this.status === true) {
+            image.setAttribute("src","heart.png")
+            this.status = false
+        } else{
+            image.setAttribute("src","heart-fill.png")
+            this.status = true
+        }
+        let try_but = document.getElementById("try")
+        try_but.replaceChild(image,try_but.childNodes[0])
     }
 
 }
 
 let main = () => {
     let try_but = document.getElementById("try")
-    try_but.onclick = render
+    try_but.onclick = ImgRender.render
 }
 main()

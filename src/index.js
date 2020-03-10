04ad5fb0a5cc325712fa86aae58a999710d4a88a
hsz1273327 <hsz1273327@gmail.com>
2020-03-10 21:36:35 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,50 +1,30 @@
-function isNull(exp){
-    return !exp && typeof exp != "undefined" && exp != 0
-}
-
-let formRender = {
-    last_md5: null,
-    target_form:document.getElementById("test_form"),
-    bindEvent: function(){
-        this.target_form.onsubmit = this.onSubmit
-        this.target_form.onreset = this.onReset
+let listFormRender = {
+    list: document.getElementById("container"),
+    target_list: document.getElementById("target_container"),
+    bindEvent: function () {
+        this.list.ondragstart = this.onDragStart
+        this.target_list.ondrop = this.onDrop
+        this.target_list.ondragover = this.onDragover
         console.log("bind")
     },
-    onSubmit: function (){
-        let table = {
-            gender:formRender.target_form.gender.value,
-            firstname:formRender.target_form.firstname.value,
-            lastname:formRender.target_form.lastname.value,
-            birthday:formRender.target_form.birthday.value,
-            email:formRender.target_form.email.value,
-            tel:formRender.target_form.tel.value,
-            homepage:formRender.target_form.homepage.value
-        }
-        let this_md5 = md5(JSON.stringify(table))
-        if (isNull(formRender.last_md5)){
-            formRender.last_md5 = this_md5
-            console.log("first table")
-            console.log(table)
-        } else{
-            if (this_md5 !== formRender.last_md5){
-                console.log("new table")
-                console.log(table)
-                formRender.last_md5 = this_md5
-            }else{
-                console.log("same input")
-            }
-        }
-        return false
+    onDragStart: function (event) {
+        event.dataTransfer.setData("TS", event.target.id)
     },
-    onReset:function (){
-        console.log(formRender.last_md5)
-        formRender.last_md5 = null
-        formRender.target_form.reset()
+    onDrop: function (event) {
+        event.preventDefault()
+        let data = event.dataTransfer.getData("TS")
+        let item = document.getElementById(data)
+        event.target.appendChild()
+        item.setAttribute("draggable",false)
+
+    },
+    onDragover: function (event) {
+        event.preventDefault()
     }
 }
 
 let main = () => {
     console.log("main")
-    formRender.bindEvent()
+    listFormRender.bindEvent()
 }
 main()

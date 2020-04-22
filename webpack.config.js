4514f8389e09b450a1e60a28e4702ca82b9acdc9
hsz1273327 <hsz1273327@gmail.com>
2020-04-22 08:43:16 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require("clean-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
     entry: './src/index.js',
@@ -73,10 +73,11 @@ module.exports = {
             template: __dirname + "/src/index.tmpl.html" //new 一个这个插件的实例，并传入相关的参数
         }),
         new webpack.HotModuleReplacementPlugin(), //热加载插件
-        new CleanWebpackPlugin('build/*.*', {
-            root: __dirname,
-            verbose: true,
-            dry: false
-        })
+        // new CleanWebpackPlugin('build/*.*', {
+        //     root: __dirname,
+        //     verbose: true,
+        //     dry: false
+        // })
+        new CleanWebpackPlugin()
     ]
 }

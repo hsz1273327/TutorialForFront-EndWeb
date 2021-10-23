21ce302271d2632d3bd8064fcd7dae885a0c6014
HUANG SIZHE <hsz1273327@gmail.com>
2021-10-23 17:46:56 +0800

    update

--- public/index.js
+++ public/index.js
@@ -1,71 +1,134 @@
 'use strict'
 
-let SpeechRecognition = webkitSpeechRecognition || SpeechRecognition
-let SpeechGrammarList = webkitSpeechGrammarList || SpeechGrammarList
-let SpeechRecognitionEvent = webkitSpeechRecognitionEvent || SpeechRecognitionEvent
-
-const colors = [ 'aqua', 'azure', 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow' ]
-const grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
-
-class SpeechRecongnitionApp {
+const Synthesis = window.speechSynthesis
+/**
+ * 录像用的前端项目
+ */
+class SpeakApplication {
+    /**
+     * 
+     */
     constructor () {
-        // 初始化语音识别接口
-        this.recognition = new SpeechRecognition()
-        this.speechRecognitionList = new SpeechGrammarList()
-        this.speechRecognitionList.addFromString(grammar, 1)
-        this.recognition.grammars = this.speechRecognitionList
-        this.recognition.lang = 'en-US'
-        this.recognition.interimResults = false
-        this.recognition.maxAlternatives = 1
+        // 控件
+        this.inputForm = document.getElementById('form')
+        this.inputTxt = document.getElementById('txt')
+        this.voiceSelect = document.getElementById('select')
+        this.pitch = document.getElementById('pitch')
+        this.pitchValue = document.getElementById('pitch-value')
+        this.rate = document.getElementById('rate')
+        this.rateValue = document.getElementById('rate-value')
 
-        //控件
-        this.diagnostic = document.querySelector('.output')
-        this.bg = document.querySelector('html')
-        this.hints = document.querySelector('.hints')
+        // 绑定事件
         this.bindEvent()
     }
-
-    bindEvent () {
-        document.body.onclick = () => this.startRecongnition()
-        this.recognition.onresult = event => this.onResult(event)
-        this.recognition.onspeechend = () => this.onSpeeched()
-        this.recognition.onnomatch = event => this.onNoMatch()
-        this.recognition.onerror = event => this.onError(event)
-    }
-
-    htmlRender () {
-        let colorHTML = ''
-        colors.forEach(function (v, i, a) {
-            console.log(v, i)
-            colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>'
+    get voices () {
+        return Synthesis.getVoices().sort((a, b) => {
+            const aname = a.name.toUpperCase()
+            const bname = b.name.toUpperCase()
+            if (aname < bname) {
+                return -1
+            } else if (aname == bname) {
+                return 0
+            } else {
+                return +1
+            }
         })
-        this.hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try ' + colorHTML + '.'
     }
-    startRecongnition () {
-        this.recognition.start()
-        console.log('Ready to receive a color command.')
+    /**
+     * 为输入输出设备和按钮绑定事件句柄的回调函数
+     */
+    bindEvent () {
+        if (speechSynthesis.onvoiceschanged !== undefined) {
+            speechSynthesis.onvoiceschanged = () => this.renderPopulateVoiceList()
+        }
+        this.inputForm.onsubmit = event => this.onStart(event)
+        this.pitch.onchange = () => this.onPitchChange()
+
+        this.rate.onchange = () => this.onRateChange()
+        this.voiceSelect.onchange = () => this.onSelectChange()
     }
-    onResult (event) {
-        let last = event.results.length - 1
-        let color = event.results[ last ][ 0 ].transcript
-        this.diagnostic.textContent = 'Result received: ' + color + '.'
-        this.bg.style.backgroundColor = color
-        console.log('Confidence: ' + event.results[ 0 ][ 0 ].confidence)
+    onStart (event) {
+        event.preventDefault()
+        this.speak()
+        this.inputTxt.blur()
     }
-    onSpeeched () {
-        this.recognition.stop()
+    onPitchChange () {
+        this.pitchValue.textContent = this.pitch.value
     }
-    onNoMatch () {
-        this.diagnostic.textContent = "I didn't recognise that color."
+    onRateChange () {
+        this.rateValue.textContent = this.rate.value
     }
-    onError (event) {
-        this.diagnostic.textContent = 'Error occurred in recognition: ' + event.error
+    onSelectChange () {
+        this.speak()
+    }
+    /**
+     * 页面初始化
+     */
+    init_render () {
+        this.renderPopulateVoiceList()
+    }
+    /**
+     * 渲染页面
+     */
+    render () {
+        this.renderPopulateVoiceList()
+    }
+    /**
+     * 渲染出语音选单
+     */
+    renderPopulateVoiceList () {
+        //渲染语音选择项
+        console.log(this.voiceSelect)
+        let selectedIndex = Object.is(this.voiceSelect.selectedIndex, undefined) ? 0 : this.voiceSelect.selectedIndex
+        this.voiceSelect.innerHTML = ''
+        for (let i = 0; i < this.voices.length; i++) {
+            let option = document.createElement('option')
+            option.textContent = this.voices[ i ].name + ' (' + this.voices[ i ].lang + ')'
+            if (this.voices[ i ].default) {
+                option.textContent += ' -- DEFAULT'
+            }
+            option.setAttribute('data-lang', this.voices[ i ].lang)
+            option.setAttribute('data-name', this.voices[ i ].name)
+            this.voiceSelect.appendChild(option)
+        }
+        this.voiceSelect.selectedIndex = selectedIndex
+    }
+
+    /**
+     * 发音
+     */
+    speak () {
+        if (Synthesis.speaking) {
+            console.error('speechSynthesis.speaking')
+            return
+        }
+        if (this.inputTxt.value !== '') {
+            let utterThis = new SpeechSynthesisUtterance(this.inputTxt.value)
+            utterThis.onend = function (event) {
+                console.log('SpeechSynthesisUtterance.onend')
+            }
+            utterThis.onerror = function (event) {
+                console.error('SpeechSynthesisUtterance.onerror')
+            }
+            let selectedOption = this.voiceSelect.selectedOptions[ 0 ].getAttribute('data-name')
+            for (let i = 0; i < this.voices.length; i++) {
+                if (this.voices[ i ].name === selectedOption) {
+                    utterThis.voice = this.voices[ i ]
+                }
+            }
+            utterThis.pitch = pitch.value
+            utterThis.rate = rate.value
+            Synthesis.speak(utterThis)
+        }
     }
 }
 
+/**
+ * 入口函数,这个入口函数是一个异步函数
+ */
 function main () {
-    let app = new SpeechRecongnitionApp()
-    app.htmlRender()
+    let app = new SpeakApplication()
+    app.init_render()
 }
 
 main()

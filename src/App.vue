9967266a4101527924801b058f3bea9f9b847db7
HUANG SIZHE <hsz1273327@gmail.com>
2021-11-01 19:14:02 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,13 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  
 });
 </script>
 

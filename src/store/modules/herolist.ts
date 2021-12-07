4e991b9b867055cb6177df658da305ae274630f9
HUANG SIZHE <hsz1273327@gmail.com>
2021-12-07 14:52:14 +0800

    update

--- src/store/modules/herolist.ts
+++ src/store/modules/herolist.ts
@@ -127,7 +127,7 @@ const actions = {
         function initEventSource(url = "http://localhost:5000/stream") {
             let evtSource = new EventSource(url, { withCredentials: true })
             evtSource.addEventListener("sync", (e: any) => {
-                console.log(e.data)
+                console.log(e)
             })
             evtSource.addEventListener("create", (e: any) => {
                 console.log(JSON.parse(e.data))

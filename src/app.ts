aff58ec05878f0b8a281669c26db3bdec4d82abd
HUANG SIZHE <hsz1273327@gmail.com>
2023-12-25 19:10:28 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import { createApp, registerElement } from 'nativescript-vue';
 import { themer } from '@nativescript-community/ui-material-core';
 import TabsPlugin from '@nativescript-community/ui-material-tabs/vue';
+import BottomNavigationBar from '@nativescript-community/ui-material-bottomnavigationbar/vue'
 
-// import Home from './views/Main_simple.vue';
-//import Home from './views/Main_simple_mf.vue';
-// import Home from './views/Main_material_tabs.vue';
-import Home from './views/Main_sb.vue';
+// import Home from './views/Main_simple.Vue';
+// import Home from './views/Main_simple_mf.Vue';
+// import Home from './views/Main_material_tabs.Vue';
+// import Home from './views/Main_sb.Vue';
+import Home from './views/Main_MDBottomNavigationBar.vue';
 
 if (global.isIOS) {
     themer.setPrimaryColor('#bff937');
@@ -13,4 +15,4 @@ if (global.isIOS) {
     themer.setSecondaryColor('#a830d7');
 }
 
-createApp(Home).use(TabsPlugin).start();
+createApp(Home).use(TabsPlugin).use(BottomNavigationBar).start();
---
+++ src/components/NavMDBottom.vue
@@ -0,0 +1,50 @@
+<template>
+    <MDBottomNavigationBar activeColor="#e57373" badgeColor="#1976d2" selectedTabIndex="0"
+        @tabSelected="onBottomNavigationTabSelected">
+        <MDBottomNavigationTab title="Home" />
+        <MDBottomNavigationTab title="Page1" />
+        <MDBottomNavigationTab title="Page2" />
+    </MDBottomNavigationBar>
+</template>
+    
+<script lang="ts" setup>
+import { $navigateTo } from "nativescript-vue";
+import { TabSelectedEventData } from "@nativescript-community/ui-material-bottomnavigationbar";
+import HomePage from "../views/HomePage.vue";
+import Page1 from "../views/Page1.vue";
+import Page2 from "../views/Page2.vue";
+
+function onBottomNavigationTabSelected(args: TabSelectedEventData) {
+    console.log("!!!!!");
+    console.log(args.oldIndex);
+    console.log(args.newIndex);
+    switch (args.newIndex) {
+        case 0:
+            {
+                $navigateTo(HomePage, {
+                    transition: { name: "fade" },
+                    frame: "main-frame",
+                });
+            }
+            break;
+        case 1:
+            {
+                $navigateTo(Page1, {
+                    transition: { name: "fade" },
+                    frame: "main-frame",
+                });
+            }
+            break;
+        case 2:
+            {
+                $navigateTo(Page2, {
+                    transition: { name: "fade" },
+                    frame: "main-frame",
+                });
+            }
+            break;
+        default:
+            console.log(`unknown index ${args.newIndex}`);
+    }
+}
+</script>

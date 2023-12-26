b90f3685f56ccc8f652513eed8ac68155010b824
HUANG SIZHE <hsz1273327@gmail.com>
2023-12-26 10:55:25 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,21 @@ import TabsPlugin from '@nativescript-community/ui-material-tabs/vue';
 import BottomNavigationBar from '@nativescript-community/ui-material-bottomnavigationbar/vue'
 import { BottomSheetPlugin } from '@nativescript-community/ui-material-bottomsheet/vue3';
 import { install as installBottomsheet } from "@nativescript-community/ui-material-bottomsheet";
+import DrawerPlugin from '@nativescript-community/ui-drawer/vue3'
+import { install as installUIDrawer} from '@nativescript-community/ui-drawer';
 
 // import Home from './views/Main_simple.Vue';
 // import Home from './views/Main_simple_mf.Vue';
 // import Home from './views/Main_material_tabs.Vue';
 // import Home from './views/Main_sb.Vue';
 // import Home from './views/Main_MDBottomNavigationBar.vue';
-import Home from './views/Main_BottomSheet.vue'
+// import Home from './views/Main_BottomSheet.vue'
+import Home from './views/Main_ui-drawer.vue'
 if (global.isIOS) {
     themer.setPrimaryColor('#bff937');
     themer.setAccentColor('#ff8a39');
     themer.setSecondaryColor('#a830d7');
 }
 installBottomsheet();
-createApp(Home).use(TabsPlugin).use(BottomNavigationBar).use(BottomSheetPlugin).start();
+installUIDrawer()
+createApp(Home).use(TabsPlugin).use(BottomNavigationBar).use(BottomSheetPlugin).use(DrawerPlugin).start();

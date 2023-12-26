fd7fbf7ee5d62f75ae322d052405de63a3e754bb
HUANG SIZHE <hsz1273327@gmail.com>
2023-12-26 11:46:38 +0800

    update

--- src/views/Main_ui-drawer.vue
+++ src/views/Main_ui-drawer.vue
@@ -1,37 +1,21 @@
 <template>
     <Frame>
         <Page>
-            <GridLayout rows="auto,*">
-                <Button text="change" @tap="onButtonTap" row="0" id="change-btn" />
-                <Frame id="main-frame" row="1">
+            <Drawer ref="drawer">
+                <SideSheet ~leftDrawer />
+                <SideSheet ~rightDrawer />
+                <NavMDBottom ~topDrawer />
+                <NavMDBottom ~bottomDrawer />
+                <Frame ~mainContent id="main-frame">
                     <HomePage />
                 </Frame>
-            </GridLayout>
+            </Drawer>
         </Page>
     </Frame>
 </template>
   
 <script lang="ts" setup>
-import { ref } from "nativescript-vue";
-import { TapGestureEventData } from "@nativescript/core/ui/gestures";
 import HomePage from "./HomePage.vue";
-import NavSegmentedBar from "../components/NavSegmentedBar.vue";
-import { useBottomSheet } from "@nativescript-community/ui-material-bottomsheet/vue3";
-
-const { showBottomSheet } = useBottomSheet()
-
-const defaultIndex = ref(0)
-function onButtonTap(evt: TapGestureEventData) {
-    showBottomSheet(NavSegmentedBar, {
-        dismissOnBackgroundTap: true,
-        props: {
-            canCloseBottomSheet: true,
-            defaultIndex: defaultIndex.value,
-        },
-        closeCallback: (...args: any[]) => {
-            console.log("bottom sheet closed", args);
-            defaultIndex.value = args[0][0][1];
-        },
-    });
-}
+import SideSheet from "../components/SideSheet.vue"
+import NavMDBottom from "../components/NavMDBottom.vue";
 </script>

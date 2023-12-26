4a0bb5321cc4be5bcc1a275f2f66c467de202700
HUANG SIZHE <hsz1273327@gmail.com>
2023-12-26 16:55:32 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,13 @@
 import { createApp } from 'nativescript-vue';
 import { themer } from '@nativescript-community/ui-material-core';
-import TabsPlugin from '@nativescript-community/ui-material-tabs/vue';
-import BottomNavigationBar from '@nativescript-community/ui-material-bottomnavigationbar/vue'
-import { BottomSheetPlugin } from '@nativescript-community/ui-material-bottomsheet/vue3';
-import { install as installBottomsheet } from "@nativescript-community/ui-material-bottomsheet";
-import DrawerPlugin from '@nativescript-community/ui-drawer/vue3'
-import { install as installUIDrawer} from '@nativescript-community/ui-drawer';
+import ActivityIndicatorPlugin from '@nativescript-community/ui-material-activityindicator/vue';
 
-// import Home from './views/Main_simple.Vue';
-// import Home from './views/Main_simple_mf.Vue';
-// import Home from './views/Main_material_tabs.Vue';
-// import Home from './views/Main_sb.Vue';
-// import Home from './views/Main_MDBottomNavigationBar.vue';
-// import Home from './views/Main_BottomSheet.vue'
-import Home from './views/Main_ui-drawer.vue'
+import Home from './views/Page_show_status.vue'
+
 if (global.isIOS) {
     themer.setPrimaryColor('#bff937');
     themer.setAccentColor('#ff8a39');
     themer.setSecondaryColor('#a830d7');
 }
-installBottomsheet();
-installUIDrawer()
-createApp(Home).use(TabsPlugin).use(BottomNavigationBar).use(BottomSheetPlugin).use(DrawerPlugin).start();
+
+createApp(Home).use(ActivityIndicatorPlugin).start();

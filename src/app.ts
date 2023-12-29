2b8767e1f131096de395c77243e36a156236e738
HUANG SIZHE <hsz1273327@gmail.com>
2023-12-29 16:49:19 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 import { createApp } from 'nativescript-vue';
-import { themer } from '@nativescript-community/ui-material-core';
+import { themer, installMixins } from '@nativescript-community/ui-material-core';
 import BottomNavigationBar from '@nativescript-community/ui-material-bottomnavigationbar/vue'
 
 import { router } from "~/router/router";
 
 import Home from './views/Main.vue'
 
+
+installMixins();
 if (global.isIOS) {
     themer.setPrimaryColor('#3F51B5');
     themer.setAccentColor('#65ADF1');
     themer.setSecondaryColor('#a830d7');
 }
+
 
 let app = createApp(Home).use(router).use(BottomNavigationBar)
 

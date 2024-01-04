a89bec851cc7d554eaa6cb6a879e7c289563d60b
HUANG SIZHE <hsz1273327@gmail.com>
2024-01-04 16:09:10 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,10 @@
       <ListView height="100%" separatorColor="transparent" :items="flicks">
         <template #default="{ item }">
           <GridLayout height="280" borderRadius="10" class="bg-secondary" rows="*, auto, auto" columns="*" margin="5 10"
-            padding="0" @tap="onFlickTap(item)">
-            <image row="0" margin="0" stretch="aspectFill" :src="item.image" />
-            <label row="1" margin="10 10 0 10" fontWeight="700" class="text-primary" fontSize="18" :text="item.title" />
-            <label row="2" margin="0 10 10 10" class="text-secondary" fontSize="14" textWrap="true"
+            padding="0" rippleColor="#FFB6C1" @tap="onFlickTap(item)">
+            <image row="0" margin="15 15 15 15" stretch="aspectFill" :src="item.image" />
+            <label row="1" margin="15 15 0 15" fontWeight="700" class="text-primary" fontSize="18" :text="item.title" />
+            <label row="2" margin="0 15 15 15" class="text-secondary" fontSize="14" textWrap="true"
               :text="item.description" />
           </GridLayout>
         </template>

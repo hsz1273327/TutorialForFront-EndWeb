e215bd3a2e5ae0511294c8921a84f17708a8e9bc
HUANG SIZHE <hsz1273327@gmail.com>
2024-01-09 17:12:56 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-const promises:Array<Promise<number>> = [
-    new Promise<number>((resolve:(value:number)=>void,reject) => resolve(1)),
-    new Promise<number>((resolve:(value:number)=>void,reject) => resolve(2)),
-    new Promise<number>((resolve:(value:number)=>void,reject) => resolve(3))
+const promises: Array<Promise<number>> = [
+    new Promise<number>((resolve: (value: number) => void, reject) => resolve(1)),
+    new Promise<number>((resolve: (value: number) => void, reject) => resolve(2)),
+    new Promise<number>((resolve: (value: number) => void, reject) => resolve(3))
 ]
 
-async function test(hello: string):Promise<void> {
+async function test(hello: string): Promise<void> {
     for await (const p of promises) {
         console.log(`${hello},${p}`)
     }

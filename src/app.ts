be7bdf3252a3ae3af3fcdf8285c70b78f69b6725
HUANG SIZHE <hsz1273327@gmail.com>
2024-01-12 10:57:33 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,19 @@
 import { createApp, registerElement } from 'nativescript-vue';
 import { themer } from '@nativescript-community/ui-material-core';
-import { enableIOSDTCoreText, Label as HTMLLabel } from '@nativescript-community/ui-label';
-import CanvasLabel from '@nativescript-community/ui-canvaslabel/vue';
-import ImageModulePlugin from '@nativescript-community/ui-image/vue';
-import { initialize as imageInitialize} from '@nativescript-community/ui-image';
-import CanvasSVG from '@nativescript-community/ui-svg/vue';
-import { Gif } from 'nativescript-gif';
+import ButtonPlugin from '@nativescript-community/ui-material-button/vue';
+import FloatingActionButtonPlugin from '@nativescript-community/ui-material-floatingactionbutton/vue';
+import speeddialPlugin from '@nativescript-community/ui-material-speeddial/vue';
 
-imageInitialize({isDownsampleEnabled: true});
+import Home from './views/Page_show_button.vue'
 
-registerElement('HTMLLabel', () => HTMLLabel);
-registerElement('Gif', () => Gif);
-
-//import Home from './views/Page_show_label.vue'
-import Home from './views/Page_show_image.vue'
-
+registerElement(
+    'Fab',
+    () => require('@nstudio/nativescript-floatingactionbutton').Fab
+)
 if (global.isIOS) {
     themer.setPrimaryColor('#bff937');
     themer.setAccentColor('#ff8a39');
     themer.setSecondaryColor('#a830d7');
-    enableIOSDTCoreText()
 }
 
-createApp(Home).use(CanvasLabel).use(ImageModulePlugin).use(CanvasSVG).start();
+createApp(Home).use(ButtonPlugin).use(FloatingActionButtonPlugin).use(speeddialPlugin).start();

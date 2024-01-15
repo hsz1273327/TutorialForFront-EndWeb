572b92433c3b76c2dab6b7ad1a3b687bd556b74a
HUANG SIZHE <hsz1273327@gmail.com>
2024-01-15 23:59:24 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,11 @@ import SliderPlugin from '@nativescript-community/ui-material-slider/vue';
 import SekkBarPlugin from '@nativescript-community/ui-range-seek-bar/vue'
 import TextFieldPlugin from '@nativescript-community/ui-material-textfield/vue'
 
-// import Home from './views/Page_show_button.vue'
-// import Home from './views/Page_show_choose.vue'
-// import Home from './views/Page_show_choose_time.vue'
-// import Home from './views/Page_show_slider.vue'
-import Home from './views/Page_show_text.vue'
+// import Home from './views/Page_input_button.vue'
+// import Home from './views/Page_input_choose.vue'
+// import Home from './views/Page_input_choose_time.vue'
+// import Home from './views/Page_input_slider.vue'
+import Home from './views/Page_input_text.vue'
 
 
 registerElement(

d4fbe2dde0be9655dfcb137c762cf938728bc22e
HUANG SIZHE <hsz1273327@gmail.com>
2024-01-15 15:27:43 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,16 @@
 import { createApp, registerElement } from 'nativescript-vue';
 import { themer } from '@nativescript-community/ui-material-core';
-import ButtonPlugin from '@nativescript-community/ui-material-button/vue';
-import FloatingActionButtonPlugin from '@nativescript-community/ui-material-floatingactionbutton/vue';
-import speeddialPlugin from '@nativescript-community/ui-material-speeddial/vue';
-import CheckBoxPlugin from '@nativescript-community/ui-checkbox/vue';
-import DateTimePickerPlugin from "@nativescript/datetimepicker/vue";
-import SliderPlugin from '@nativescript-community/ui-material-slider/vue';
-import SekkBarPlugin from '@nativescript-community/ui-range-seek-bar/vue'
-import TextFieldPlugin from '@nativescript-community/ui-material-textfield/vue'
-
-// import Home from './views/Page_show_button.vue'
-// import Home from './views/Page_show_choose.vue'
-// import Home from './views/Page_show_choose_time.vue'
-// import Home from './views/Page_show_slider.vue'
-import Home from './views/Page_show_text.vue'
 
 
-registerElement(
-    'Fab',
-    () => require('@nstudio/nativescript-floatingactionbutton').Fab
-)
+import Home from './views/Page_show_html.vue'
+// import Home from './views/Page_show_webview.vue'
+// import Home from './views/Page_show_ui_webview.vue'
+
+
 if (global.isIOS) {
     themer.setPrimaryColor('#bff937');
     themer.setAccentColor('#ff8a39');
     themer.setSecondaryColor('#a830d7');
 }
 
-createApp(Home).use(ButtonPlugin).use(FloatingActionButtonPlugin).use(speeddialPlugin).use(CheckBoxPlugin).use(DateTimePickerPlugin).use(SliderPlugin).use(SekkBarPlugin).use(TextFieldPlugin).start();
+createApp(Home).start();

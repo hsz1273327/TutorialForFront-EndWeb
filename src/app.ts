f28303b99b77f4f951ee1d04f08c714630706713
HUANG SIZHE <hsz1273327@gmail.com>
2024-01-17 15:53:40 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,18 @@ import ImageModulePlugin from '@nativescript-community/ui-image/vue';
 import { initialize as imageInitialize} from '@nativescript-community/ui-image';
 import CanvasSVG from '@nativescript-community/ui-svg/vue';
 import { Gif } from 'nativescript-gif';
+import { BlurView } from '@nativescript-community/ui-blurview';
+
+
 
 imageInitialize({isDownsampleEnabled: true});
 
 registerElement('HTMLLabel', () => HTMLLabel);
 registerElement('Gif', () => Gif);
+registerElement('BlurView', () => BlurView);
 
-import Home from './views/Page_show_label.vue'
-//import Home from './views/Page_show_image.vue'
+// import Home from './views/Page_show_label.vue'
+import Home from './views/Page_show_image.vue'
 
 if (global.isIOS) {
     themer.setPrimaryColor('#bff937');

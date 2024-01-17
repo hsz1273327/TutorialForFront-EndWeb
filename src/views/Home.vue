7bd7c477f8ce8180b25fb05e75eba4b74d6c7eba
HUANG SIZHE <hsz1273327@gmail.com>
2024-01-17 16:07:18 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,10 @@
             padding="0" rippleColor="#FFB6C1" @tap="onFlickTap(item)">
             
             <image row="0" margin="15 15 15 15" stretch="aspectFill" :src="item.image" />
-            <BlurView blurRadius="10" width="100%" height="200"></BlurView>
             <label row="1" margin="15 15 0 15" fontWeight="700" class="text-primary" fontSize="18" :text="item.title" />
             <label row="2" margin="0 15 15 15" class="text-secondary" fontSize="14" textWrap="true"
               :text="item.description" />
+            <BlurView blurRadius="10" width="100%" height="200"></BlurView>
           </GridLayout>
         </template>
       </ListView>

0b49f818d0d1707cc6683b51a64d13a76e017ffe
HUANG SIZHE <hsz1273327@gmail.com>
2024-01-22 17:03:48 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,12 @@ import { themer } from '@nativescript-community/ui-material-core';
 import CardViewPlugin from '@nativescript-community/ui-material-cardview/vue';
 import ImageModulePlugin from '@nativescript-community/ui-image/vue';
 import { initialize as imageInitialize} from '@nativescript-community/ui-image';
+import CollectionView from '@nativescript-community/ui-collectionview/vue3';
 
+import Home from './views/CollectionView_Home.vue'
+// import Home from './views/CollectionView_Home.vue'
+// import Home from './views/CollectionView_Home.vue'
 
-import Home from './views/Home.vue'
 if (global.isIOS) {
     themer.setPrimaryColor('#bff937');
     themer.setAccentColor('#ff8a39');
@@ -13,4 +16,4 @@ if (global.isIOS) {
 }
 
 imageInitialize({isDownsampleEnabled: true});
-createApp(Home).use(CardViewPlugin).use(ImageModulePlugin).start();
+createApp(Home).use(CardViewPlugin).use(ImageModulePlugin).use(CollectionView).start();
---
+++ src/views/CollectionView_Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <frame>
+        <Page actionBarHidden="true">
+            <CollectionView :items="itemList" colWidth="50%" rowHeight="100" @itemTap="tapItem">
+                <template #default="{ item }">
+                    <StackLayout :backgroundColor="item.color">
+                        <Label :text="item.name" />
+                    </StackLayout>
+                </template>
+            </CollectionView>
+        </Page>
+    </frame>
+</template>
+<script lang="ts" setup>
+import { ref } from "nativescript-vue";
+import { ObservableArray } from '@nativescript/core';
+import { CollectionViewItemEventData } from "@nativescript-community/ui-collectionview"
+
+
+const itemList = ref(new ObservableArray([
+    { name: 'TURQUOISE', color: '#1abc9c' },
+    { name: 'EMERALD', color: '#2ecc71' },
+    { name: 'PETER RIVER', color: '#3498db' },
+    { name: 'AMETHYST', color: '#9b59b6' },
+    { name: 'WET ASPHALT', color: '#34495e' }
+]));
+
+function tapItem(evt: CollectionViewItemEventData) {
+    console.log(`tap item with index ${evt.index}`)
+}
+</script>

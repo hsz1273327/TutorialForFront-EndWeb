3f458f263cb65b42af979a179de05ba1ad2c837d
HUANG SIZHE <hsz1273327@gmail.com>
2024-01-22 15:58:00 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,16 @@
 import { createApp } from 'nativescript-vue';
 import { themer } from '@nativescript-community/ui-material-core';
-import TabsPlugin from '@nativescript-community/ui-material-tabs/vue';
-import BottomNavigationBar from '@nativescript-community/ui-material-bottomnavigationbar/vue'
-import { BottomSheetPlugin } from '@nativescript-community/ui-material-bottomsheet/vue3';
-import { install as installBottomsheet } from "@nativescript-community/ui-material-bottomsheet";
-import ButtonPlugin from '@nativescript-community/ui-material-button/vue';
-import DrawerPlugin from '@nativescript-community/ui-drawer/vue3'
-import { install as installUIDrawer} from '@nativescript-community/ui-drawer';
+import CardViewPlugin from '@nativescript-community/ui-material-cardview/vue';
 import ImageModulePlugin from '@nativescript-community/ui-image/vue';
 import { initialize as imageInitialize} from '@nativescript-community/ui-image';
 
-// import Home from './views/Main_simple.vue';
-// import Home from './views/Main_simple_mf.vue';
-// import Home from './views/Main_material_tabs.vue';
-// import Home from './views/Main_sb.vue';
-// import Home from './views/Main_MDBottomNavigationBar.vue';
-import Home from './views/Main_BottomSheet.vue'
-// import Home from './views/Main_ui-drawer.vue'
+
+import Home from './views/Home.vue'
 if (global.isIOS) {
     themer.setPrimaryColor('#bff937');
     themer.setAccentColor('#ff8a39');
     themer.setSecondaryColor('#a830d7');
 }
-installBottomsheet();
-installUIDrawer()
+
 imageInitialize({isDownsampleEnabled: true});
-createApp(Home).use(TabsPlugin).use(BottomNavigationBar).use(ButtonPlugin).use(BottomSheetPlugin).use(DrawerPlugin).use(ImageModulePlugin).start();
+createApp(Home).use(CardViewPlugin).use(ImageModulePlugin).start();
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <frame>
+        <Page actionBarHidden="true">
+            <GridLayout rows="*,auto">
+                <SimpleCard v-bind="simplecardprops" />
+                <Button row="1" text="outside" />
+            </GridLayout>
+        </Page>
+    </frame>
+</template>
+<script lang="ts" setup>
+import { ref } from "nativescript-vue";
+import SimpleCard from "../components/SimpleCard.vue"
+
+const simplecardprops = ref({
+    title: "test1",
+    iconImgURL: "~/assets/anastasia.png",
+    mediaImgURL: "~/assets/beetlejuicemusical.png",
+    description: "this is test's description"
+})
+
+</script>

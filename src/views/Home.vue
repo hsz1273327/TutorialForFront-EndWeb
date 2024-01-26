3123a5e1f9004799823bd4d28f5f4869b7c2b47d
HUANG SIZHE <hsz1273327@gmail.com>
2024-01-26 18:41:04 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@
             <StackLayout>
                 <DrawingPad ref="drawingpad" height="400" id="drawingPad" penColor="#ff4081" penWidth="3">
                 </DrawingPad>
-                <Image v-show="image_source" ref="img"></Image>
+                <Image v-show="show" ref="img"></Image>
             </StackLayout>
 
             <!-- <ListView ref="collection" height="100%" separatorColor="transparent" :items="itemList" colWidth="50%"
@@ -39,27 +39,28 @@ const fontClean = "font://\uf1c3"
 
 const drawingpad = ref()
 const img = ref()
+const show = ref(false)
 
 function getMyDrawing(evt: EventData) {
     // get reference to the drawing pad
     const pad = drawingpad.value.$el.nativeView
 
     // then get the drawing (Bitmap on Android) of the drawingpad
-    let drawingImage;
+    // let drawingImage;
     pad.getDrawing().then((data) => {
         console.log(data);
         // At this point you have a native image (Bitmap on Android or UIImage on iOS)
         // so lets convert to a NS Image using the ImageSource
         const image = new ImageSource(data); // this can be set as the `src` of an `Image` inside your NS
-        drawingImage = image; // to set the src of an Image if needed.
+        // drawingImage = image; // to set the src of an Image if needed.
+        img.value.src = image
+        show.value = true
         // // now you might want a base64 version of the image
         // const base64imageString = image.toBase64String('jpg'); // if you need it as base64
         // console.log('::IMG_BASE64::', base64imageString);
     }).catch((err) => {
         console.log(err);
-    }
-
-    )
+    })
 }
 
 function clearMyDrawing(evt: EventData) {

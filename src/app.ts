75b14ece954efe769247cfc6904b835fed82e8a7
HUANG SIZHE <hsz1273327@gmail.com>
2024-02-20 14:25:39 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ import { themer } from '@nativescript-community/ui-material-core';
 import CollectionView from '@nativescript-community/ui-collectionview/vue3';
 import waterfallInstall from '@nativescript-community/ui-collectionview-waterfall';
 
-
+import Home from './views/ScrollView_Home.vue'
 // import Home from './views/ListView_Home.vue'
 // import Home from './views/CollectionView_Home.vue'
-import Home from './views/Waterfall_Home.vue'
+// import Home from './views/Waterfall_Home.vue'
 
 if (global.isIOS) {
   themer.setPrimaryColor('#bff937');
---
+++ src/views/ScrollView_Home.vue
@@ -0,0 +1,46 @@
+<template>
+    <frame>
+        <Page actionBarHidden="true">
+            <ScrollView>
+                <StackLayout padding="12">
+                    <template v-for="(item, key) in itemList" :key="key">
+                        <StackLayout :backgroundColor="item.color" height="100">
+                            <Label :text="item.name" />
+                        </StackLayout>
+                    </template>
+                </StackLayout>
+            </ScrollView>
+        </Page>
+    </frame>
+</template>
+<script lang="ts" setup>
+import { ref } from "nativescript-vue";
+
+interface Card {
+    name: string
+    color: string
+}
+const itemList = ref<Card[]>([
+    { name: 'TURQUOISE', color: '#1abc9c' },
+    { name: 'EMERALD', color: '#2ecc71' },
+    { name: 'PETER RIVER', color: '#3498db' },
+    { name: 'AMETHYST', color: '#9b59b6' },
+    { name: 'WET ASPHALT', color: '#34495e' },
+    { name: 'GREEN SEA', color: '#16a085' },
+    { name: 'NEPHRITIS', color: '#27ae60' },
+    { name: 'BELIZE HOLE', color: '#2980b9' },
+    { name: 'WISTERIA', color: '#8e44ad' },
+    { name: 'MIDNIGHT BLUE', color: '#2c3e50' },
+    { name: 'SUN FLOWER', color: '#f1c40f' },
+    { name: 'CARROT', color: '#e67e22' },
+    { name: 'ALIZARIN', color: '#e74c3c' },
+    { name: 'CLOUDS', color: '#ecf0f1' },
+    { name: 'CONCRETE', color: '#95a5a6' },
+    { name: 'ORANGE', color: '#f39c12' },
+    { name: 'PUMPKIN', color: '#d35400' },
+    { name: 'POMEGRANATE', color: '#c0392b' },
+    { name: 'SILVER', color: '#bdc3c7' },
+    { name: 'ASBESTOS', color: '#7f8c8d' }
+]);
+
+</script>

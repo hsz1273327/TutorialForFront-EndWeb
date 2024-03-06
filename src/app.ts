5392b4487e56926ca13718aef1cbc6867d44d301
HUANG SIZHE <hsz1273327@gmail.com>
2024-03-06 14:46:08 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,4 @@
 import { createApp } from 'nativescript-vue';
 import Home from './views/Home.vue'
-import { LocalNotifications, ReceivedNotification } from '@nativescript/local-notifications'
-
-LocalNotifications.addOnMessageReceivedCallback((notification: ReceivedNotification) => {
-    //Handle the received notification
-    console.log(`MessageReceived title ${notification.title}`);
-}).then(() => {
-    console.log('Listener added');
-});
 
 createApp(Home).start();

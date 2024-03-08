34d338898d45b73401c292b5e87ea723004c1a21
HUANG SIZHE <hsz1273327@gmail.com>
2024-03-08 22:56:47 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,7 @@
 import { createApp, registerElement } from 'nativescript-vue';
-import { BottomSheetPlugin } from '@nativescript-community/ui-material-bottomsheet/vue3';
-import { install as installBottomsheet } from "@nativescript-community/ui-material-bottomsheet";
-installBottomsheet();
-
 
 import Home from './views/HomeCamera.vue'
 // import Home from './views/HomeVideoRecorder.vue'
 
 registerElement('VideoPlayer', () => require('nativescript-videoplayer').Video)
-registerElement('BarcodeView', () => require('@nativescript-community/ui-barcodeview').BarcodeView)
-createApp(Home).use(BottomSheetPlugin).start();
+createApp(Home).start();
--- src/views/HomeCamera.vue
+++ src/views/HomeCamera.vue
@@ -5,15 +5,8 @@
                 <Label :text="cam_available_msg" />
                 <Button v-show="!cam_available" text="申请摄像头授权" @tap="queryCamPerm" />
                 <Button v-show="cam_available" text="拍张照片" @tap="takePhoto" />
+                <!-- <Button text="拍张照片" @tap="takePhoto" /> -->
                 <Image v-show="has_photo" ref="photoimg" />
-                <Button class="btn btn-primary btn-rounded-sm" text="点击扫码" @tap="showQRScaner"></Button>
-                <Button class="btn btn-primary btn-rounded-sm" text="生成二维码" @tap="generateBarcode"></Button>
-                <Image v-show="generatedBarcodeText" ref="generatedBarcode" width="140" height="140"
-                    horizontalAlignment="center" verticalAlignment="center" backgroundColor="red" />
-                <Label v-show="generatedBarcodeText" class="body" textAlignment="center" textWrap="true">
-                    <Span text="read/generated barcode: " />
-                    <Span fontWeight="bold" :text="generatedBarcodeText" />
-                </Label>
             </StackLayout>
         </Page>
     </Frame>
@@ -23,9 +16,6 @@
 import { ref, computed } from 'nativescript-vue'
 import { Image } from "@nativescript/core"
 import { isAvailable, requestPermissions, takePicture } from '@nativescript/camera'
-import { useBottomSheet } from "@nativescript-community/ui-material-bottomsheet/vue3";
-import { generateBarCode, BarcodeFormat } from '@nativescript-community/ui-barcodeview';
-import QRScaner from "../components/QRScaner.vue";
 
 const cam_available = ref(false)
 const photoimg = ref()
@@ -39,10 +29,12 @@ const has_photo = ref(false)
 function check_cam_status() {
     let status = isAvailable()
     cam_available.value = status.valueOf()
+    console.log(`cam_available is ${cam_available.value}`)
 }
 
 async function queryCamPerm() {
-    await requestPermissions()
+    let result = await requestPermissions()
+    console.log(`requestPermissions ok ${result}`)
     check_cam_status()
 }
 
@@ -62,49 +54,5 @@ async function takePhoto() {
     } catch (error) {
         console.log("Error -> " + error.message)
     }
-}
-
-const generatedBarcodeText = ref()
-const generatedBarcode = ref()
-
-function generateBarcode() {
-    let text = 'ecairn://transfer/443427876#44#Le Caméléon Nicolas'
-    let type: BarcodeFormat = 'QR_CODE'
-    let imgsource = generateBarCode({
-        text: text,
-        type: type,
-        width: 400,
-        height: 400,
-        frontColor: 'green',
-        backColor: 'yellow'
-    })
-    console.log('************generated barcode image')
-    let img = generatedBarcode.value._nativeView as Image
-    img.src = imgsource
-    generatedBarcodeText.value = text
-    console.log('****************set qrcode img')
-}
-const { showBottomSheet } = useBottomSheet()
-function showQRScaner() {
-    showBottomSheet(QRScaner, {
-        closeCallback: (...args) => {
-            if (args.length === 2 && typeof (args[0]) == "string") {
-                console.log(`bottom sheet closed with scan result ${args[0]} ,${args[1]}`)
-                let img = generatedBarcode.value._nativeView as Image
-                let imgsource = generateBarCode({
-                    text: args[0],
-                    type: args[1],
-                    width: 400,
-                    height: 400,
-                    frontColor: 'green',
-                    backColor: 'yellow'
-                });
-                img.src = imgsource
-                generatedBarcodeText.value = args[0]
-                console.log('generated barcode image')
-
-            }
-        }
-    });
 }
 </script>

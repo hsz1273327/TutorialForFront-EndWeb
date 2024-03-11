9e8a466705b769abd0aee0bb35c7f9dc46c18237
HUANG SIZHE <hsz1273327@gmail.com>
2024-03-11 16:14:11 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import { createApp, registerElement } from 'nativescript-vue';
+// import { WebRTC, WebRTCView} from 'nativescript-webrtc-plugin';
+// WebRTC.init(); // <= Try calling this in you app.js or app.ts or main.ts
 
 // import Home from './views/nativeHttpHome.vue'
 // import Home from "./views/communityHttpHome.vue"
 // import Home from "./views/wsHome.vue"
 import Home from "./views/mqttHome.vue"
+// import Home from "./views/webrtcMsgHome.vue"
 
 import '@valor/nativescript-websockets'
 
@@ -11,6 +14,11 @@ registerElement(
     'PullToRefresh',
     () => require('@nativescript-community/ui-pulltorefresh').PullToRefresh
 )
+
+// registerElement(
+//     'WebRTCView',
+//     () => WebRTCView
+// )
 // bghttp_init()
 createApp(Home).start();
 

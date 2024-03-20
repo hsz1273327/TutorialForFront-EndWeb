b6896eb14a1a91fe0210d7d23df50ca210817828
HUANG SIZHE <hsz1273327@gmail.com>
2024-03-20 17:15:01 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,5 @@
 import { createApp } from 'nativescript-vue';
 import Home from './views/Home.vue'
-import { Application, ApplicationEventData } from '@nativescript/core'
-
-Application.on('launch', (evt: ApplicationEventData) => {
-    console.log(`application get launch event: eventName:${evt.eventName} android:${evt.android} ios:${evt.ios}`)
-})
-
-Application.on('suspend', (evt: ApplicationEventData) => {
-    console.log(`application get suspend event: eventName:${evt.eventName} android:${evt.android} ios:${evt.ios}`)
-})
-Application.on('resume', (evt: ApplicationEventData) => {
-    console.log(`application get resume event: eventName:${evt.eventName} android:${evt.android} ios:${evt.ios}`)
-})
-
 
 createApp(Home).start();
 

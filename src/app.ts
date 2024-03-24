f5db20c3c3813f7a777aad27fc8e4e78417ed979
HUANG SIZHE <hsz1273327@gmail.com>
2024-03-25 02:08:26 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import TabsPlugin from '@nativescript-community/ui-material-tabs/vue';
 import BottomNavigationBar from '@nativescript-community/ui-material-bottomnavigationbar/vue'
 import DrawerPlugin from '@nativescript-community/ui-drawer/vue3'
 import { install as installUIDrawer} from '@nativescript-community/ui-drawer'
+import ImageModulePlugin from '@nativescript-community/ui-image/vue';
+import { initialize as imageInitialize} from '@nativescript-community/ui-image';
+
 
 // import Home from './views/ActionBarHome.vue';
 // import Home from './views/TabHome.vue';
-// import Home from './views/TabFrameHome.vue';
 // import Home from './views/MaterialTabHome.vue';
 // import Home from './views/SegmentedBarHome.vue';
 import Home from './views/MDBottomNavigationBarHome.vue';
@@ -17,7 +19,8 @@ if (global.isIOS) {
     themer.setAccentColor('#ff8a39');
     themer.setSecondaryColor('#a830d7');
 }
+imageInitialize({isDownsampleEnabled: true});
 
 installUIDrawer()
 
-createApp(Home).use(TabsPlugin).use(BottomNavigationBar).use(DrawerPlugin).start();
+createApp(Home).use(ImageModulePlugin).use(TabsPlugin).use(BottomNavigationBar).use(DrawerPlugin).start();

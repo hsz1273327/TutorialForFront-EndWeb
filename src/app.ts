96ccfc6a6e3d420faa8eb893a1b4ef961c7664fb
HUANG SIZHE <hsz1273327@gmail.com>
2024-04-09 11:11:30 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,6 @@
 import { createApp } from 'nativescript-vue'
-import { BottomSheetPlugin } from '@nativescript-community/ui-material-bottomsheet/vue3'
-import { install as installBottomsheet } from "@nativescript-community/ui-material-bottomsheet"
-installBottomsheet()
 import Home from './views/Home.vue'
 
-createApp(Home).use(BottomSheetPlugin).start()
+createApp(Home).start()
 
 // createApp(Home).use(ImageModulePlugin).use(CanvasSVG).start();

ee8e311d03899415b0cb5abaeb21935b40be9b25
HUANG SIZHE <hsz1273327@gmail.com>
2024-04-10 11:35:37 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'nativescript-vue'
-
 import Home from './views/Home.vue'
+import { handleOpenURL, AppURL } from '@nativescript-community/appurl'
+
+handleOpenURL((appURL: AppURL) => {
+    console.log('Got the following appURL', appURL);
+});
 
 createApp(Home).start()
 

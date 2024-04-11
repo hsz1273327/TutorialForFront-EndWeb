519e5e38cf8d69daa9087581c80752c9ceb46ebd
HUANG SIZHE <hsz1273327@gmail.com>
2024-04-11 11:27:29 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import { createApp } from 'nativescript-vue'
 import Home from './views/Home.vue'
-import { handleOpenURL, AppURL } from '@nativescript-community/appurl'
+// import { handleOpenURL, AppURL } from '@nativescript-community/appurl'
+import { registerUniversalLinkCallback, getUniversalLink } from "@nativescript-community/universal-links";
+import * as urlparse from 'url-parse';
 
-handleOpenURL((appURL: AppURL) => {
-    console.log('Got the following appURL', appURL);
+// handleOpenURL((appURL: AppURL) => {
+//     console.log('Got the following appURL', appURL);
+// });
+
+const ul = getUniversalLink()
+
+console.log(`getUniversalLink ${ul}`)
+
+registerUniversalLinkCallback((ul: string) => {
+    // use the router to navigate to the screen
+    console.log(`get url ${ul}`)
+    const url = urlparse(ul, true)
+    console.log(`url parsed as ${JSON.stringify(url)}`)
 });
 
 createApp(Home).start()
-
-// createApp(Home).use(ImageModulePlugin).use(CanvasSVG).start();

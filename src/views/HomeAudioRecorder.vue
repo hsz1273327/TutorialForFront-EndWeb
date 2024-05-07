edac97130e258184861c303e3e8703ace874da46
HUANG SIZHE <hsz1273327@gmail.com>
2024-05-07 23:08:22 +0800

    update

--- src/views/HomeAudioRecorder.vue
+++ src/views/HomeAudioRecorder.vue
@@ -4,8 +4,7 @@
             <StackLayout>
                 <Label :text="audiorecorder_available_msg" />
                 <Button v-show="!audiorecorder_available" text="申请麦克风授权" @tap="queryPerm" />
-                <Button v-show="audiorecorder_available" :text="recordbtntxt" @tap="record" />
-
+                <Button v-show="audiorecorder_available" :text="recordbtntxt" @tap="recordOrPlayer" />
             </StackLayout>
         </Page>
     </Frame>
@@ -13,20 +12,24 @@
 
 <script lang="ts" setup>
 import { ref, computed } from 'nativescript-vue'
-import { knownFolders, isAndroid } from '@nativescript/core'
+import { knownFolders, isAndroid,path } from '@nativescript/core'
 import { TNSRecorder, AudioRecorderOptions } from '@nativescript-community/audio';
 // import { Video as VideoPlayer} from 'nativescript-videoplayer'
 const audiorecorder_available = ref(false)
-
 const audiorecorder_isRecording = ref(false)
-
+const audiorecorder_hasRecorder = ref(false)
+const audiorecorder_isPlaying = ref(false)
 const recorder = new TNSRecorder();
 const audiorecorder_available_msg = computed(() => {
     return audiorecorder_available.value ? '有麦克风授权' : '没有麦克风授权'
 })
 
 const recordbtntxt = computed(() => {
-    return audiorecorder_isRecording.value ? '停止录音' : '开始录音'
+    if (audiorecorder_hasRecorder.value){
+        return audiorecorder_isPlaying.value? '停止播放':'开始播放'
+    }else{
+        return audiorecorder_isRecording.value ? '停止录音' : '开始录音'
+    }
 })
 
 function check_recorder_status() {
@@ -38,7 +41,9 @@ async function queryPerm() {
     await recorder.requestRecordPermission()
     check_recorder_status()
 }
-
+const platformExtension = isAndroid?'m4a' : 'caf' //mp3
+const audioFolder = knownFolders.externalDocuments().getFolder('audio')
+const recordingPath = path.join(audioFolder.path,`recording.`)`${audioFolder.path}/recording.${platformExtension}`
 async function record() {
     if (audiorecorder_isRecording.value) {
         await recorder.stop()
@@ -48,8 +53,7 @@ async function record() {
             console.log('This device cannot record audio.')
             return
         }
-        const audioFolder = knownFolders.externalDocuments().getFolder('audio')
-        const recordingPath = `${audioFolder.path}/recording.${this.platformExtension()}`
+        
         const recorderOptions: AudioRecorderOptions = {
             filename: recordingPath,
             sampleRate: 48000,
@@ -77,10 +81,11 @@ async function record() {
                 }
             })
         }
-
-        await recorder.start(recorderOptions)
         audiorecorder_isRecording.value = true
+        await recorder.start(recorderOptions)
     }
 }
+
+async function play()
 
 </script>

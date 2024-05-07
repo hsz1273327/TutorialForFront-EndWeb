adb9b931cc3af3c919998cc8f682e1aa5025779a
HUANG SIZHE <hsz1273327@gmail.com>
2024-05-07 16:34:31 +0800

    update

---
+++ src/components/ModalView.vue
@@ -0,0 +1,25 @@
+<template>
+    <Stacklayout backgroundColor="white" padding="25">
+        <Button :text="info" fontSize="40"  borderColor="green" @tap="closeModal" />
+    </Stacklayout>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "nativescript-vue";
+import { EventData,Button } from '@nativescript/core'
+const props = defineProps({
+    member: {
+        type: String,
+        required: true
+    },
+})
+
+const info = `这是 ${props.member}`
+
+function closeModal(evt: EventData) {
+    const button = evt.object as Button
+    button.closeModal({
+        name: 'a message',
+    })
+}
+</script>
---
+++ src/views/ModalHome.vue
@@ -0,0 +1,27 @@
+<template>
+    <Frame>
+        <Page actionBarHidden="true">
+            <StackLayout>
+                <Button ref="b1" text="test modal view should work" borderColor="red" @tap="openModal" />
+            </StackLayout>
+        </Page>
+    </Frame>
+</template>
+
+<script lang="ts" setup>
+import { $showModal } from 'nativescript-vue'
+import { EventData } from '@nativescript/core'
+import ModalView from '../components/ModalView.vue'
+
+
+async function openModal(evt: EventData) {
+    console.log('openModal')
+    let msg_back = await $showModal(ModalView, {
+        fullscreen: false,
+        props: {
+            member: 'test modal'
+        }
+    })
+    console.log(`get msg ${JSON.stringify(msg_back)}`)
+}
+</script>

bf2ed904d8c3e378ba8debf500b96bb92e8f968b
HUANG SIZHE <hsz1273327@gmail.com>
2024-05-08 10:40:24 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,23 +2,90 @@
     <Frame>
         <Page actionBarHidden="true">
             <StackLayout>
-                <Slider v-model="value" @valueChange="onValueChanged" />
+                <ListPicker :items="listOfItems" selectedIndex="0" @selectedIndexChange="pickChange" />
+                <Button text="impact-LIGHT" @tap="impact(1)" />
+                <Button text="impact-MEDIUM" @tap="impact(2)" />
+                <Button text="impact-HEAVY" @tap="impact(3)" />
+                <Button text="notification-ERROR" @tap="notification_error" />
+                <Button text="notification-WARNING" @tap="notification_warning" />
+                <Button text="notification-SUCCESS" @tap="notification_success" />
             </StackLayout>
         </Page>
     </Frame>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'nativescript-vue'
-import { PropertyChangeData } from '@nativescript/core/data/observable'
-import { Brightness } from '@nativescript/brightness'
-const brightness = new Brightness()
-const defaultValue = brightness.get()
-const value = ref(defaultValue)
+import { ref, onMounted } from 'nativescript-vue'
+import { EventData, EventDataValue } from '@nativescript/core/data/observable'
+import { Feedback, FeedbackPosition } from "nativescript-feedback"
+import { Haptics,HapticNotificationType } from '@nativescript/haptics'
 
-function onValueChanged(evt: PropertyChangeData) {
-    console.log(`old brightness is ${ evt.oldValue}`)
-    brightness.set({ intensity: evt.value })
-    console.log(`now brightness is ${ evt.value}`)
+const feedback = new Feedback();
+const isSupported = ref(false)
+const listOfItems = ref(["a", "b", "c"])
+
+function pickChange(evt: EventDataValue) {
+    console.log(`pick index ${evt.value}`)
+    if (isSupported.value) {
+        Haptics.selection()
+    }
 }
+
+function impact(level: number) {
+    if (isSupported.value) {
+        if ((level > 3) || (level < 1)) {
+            Haptics.impact()
+        } else {
+            Haptics.impact(level)
+        }
+    }
+}
+
+function notification_error(evt: EventData) {
+    feedback.error({
+        message: "a error",
+        position: FeedbackPosition.Top
+    })
+    if (isSupported.value) {
+        Haptics.notification(HapticNotificationType.ERROR)
+    }
+    console.log("notification_error ok")
+}
+function notification_warning(evt: EventData) {
+    feedback.warning({
+        message: "a warning",
+        position: FeedbackPosition.Top
+    })
+    if (isSupported.value) {
+        Haptics.notification(HapticNotificationType.WARNING)
+    }
+    console.log("notification_warning ok")
+}
+function notification_success(evt: EventData) {
+    feedback.success({
+        message: "a success",
+        position: FeedbackPosition.Top
+    })
+    if (isSupported.value) {
+        Haptics.notification(HapticNotificationType.SUCCESS)
+    }
+    console.log("notification_success ok")
+}
+
+onMounted(() => {
+    if (Haptics.isSupported()) {
+        feedback.info({
+            message: "Haptics is Supported",
+            position: FeedbackPosition.Top
+        })
+        isSupported.value = true
+        console.log("Haptics is Supported")
+    } else {
+        feedback.warning({
+            message: "Haptics is not Supported",
+            position: FeedbackPosition.Top
+        })
+        console.log("Haptics is not Supported")
+    }
+})
 </script>

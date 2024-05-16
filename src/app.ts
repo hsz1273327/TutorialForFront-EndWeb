aa4a044fd3640be22793f8ba1fb8abf9026b78af
HUANG SIZHE <hsz1273327@gmail.com>
2024-05-16 14:12:37 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,27 @@
-import { createApp } from 'nativescript-vue'
+import { createApp,ref } from 'nativescript-vue'
 import Home from './views/Home.vue'
+// import { createPinia } from 'pinia'
+import { Application, isAndroid, Device } from '@nativescript/core'
+import { androidLaunchEventLocalizationHandler } from '@nativescript/localize'
+// import { useLanguageStore } from './store/language'
+let app = createApp(Home)
+const languageChangedMessage = ref("")
+app.provide(/* 注入名 */ 'languageChangedMessage', /* 值 */ languageChangedMessage)
+Application.on(Application.launchEvent, (args) => {
+    if (args.android) {
+        try {
+            androidLaunchEventLocalizationHandler()
+            // const store = useLanguageStore()
+            // const { languageChange } = store
+            let nowlanguage = Device.language.split('-')[0]
+            languageChangedMessage.value="language changed"
+            console.log(`androidLaunchEventLocalizationHandler languageChanged`)
+        } catch (error) {
+            console.log(`androidLaunchEventLocalizationHandler get error ${error}`)
+        }
+    }
+})
 
-createApp(Home).start()
+
+
+app.start() //.use(createPinia())
---
+++ src/components/supportedLanguageModal.vue
@@ -0,0 +1,36 @@
+<template>
+    <Stacklayout backgroundColor="white" padding="25">
+        <ListView ref="collection" height="500" separatorColor="transparent" :items="members" colWidth="80%"
+            rowHeight="100" @itemTap="chooseOne">
+            <template #default="{ item }">
+                <StackLayout height="100">
+                    <Label :text="item.name" />
+                </StackLayout>
+            </template>
+        </ListView>
+    </Stacklayout>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "nativescript-vue";
+import { ItemEventData, ListView } from '@nativescript/core'
+
+const members = ref([
+    { "name": "简体中文(中国大陆)", "symbol": "zh-CN" },
+    { "name": "简体中文(新加坡)", "symbol": "zh-SG" },
+    { "name": "United States English", "symbol": "en-US" },
+    { "name": "United Kingdom English", "symbol": "en-GB" },
+    { "name": "Australia English", "symbol": "en-AU" },
+    { "name": "Canada English", "symbol": "en-CA" },
+    { "name": "New Zealand English", "symbol": "en-NZ" },
+    { "name": "Ireland English", "symbol": "en-IE" },
+    { "name": "South Africa English", "symbol": "en-ZA" }
+])
+
+function chooseOne(evt: ItemEventData) {
+    let item = members.value[evt.index]
+    console.log(`choose : ${item.name}`)
+    const lv = evt.object as ListView
+    lv.closeModal(item.symbol)
+}
+</script>

2dfde87fbaf4db203c4ea1ab9da4b2d638cdcf9f
HUANG SIZHE <hsz1273327@gmail.com>
2024-05-16 17:55:58 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,16 @@
 import { createApp,ref } from 'nativescript-vue'
 import Home from './views/Home.vue'
-// import { createPinia } from 'pinia'
 import { Application, isAndroid, Device } from '@nativescript/core'
 import { androidLaunchEventLocalizationHandler } from '@nativescript/localize'
-// import { useLanguageStore } from './store/language'
+
 let app = createApp(Home)
 const languageChangedMessage = ref("")
 app.provide(/* 注入名 */ 'languageChangedMessage', /* 值 */ languageChangedMessage)
+
 Application.on(Application.launchEvent, (args) => {
     if (args.android) {
         try {
             androidLaunchEventLocalizationHandler()
-            // const store = useLanguageStore()
-            // const { languageChange } = store
             let nowlanguage = Device.language.split('-')[0]
             languageChangedMessage.value="language changed"
             console.log(`androidLaunchEventLocalizationHandler languageChanged`)
@@ -22,6 +20,4 @@ Application.on(Application.launchEvent, (args) => {
     }
 })
 
-
-
-app.start() //.use(createPinia())
+app.start()

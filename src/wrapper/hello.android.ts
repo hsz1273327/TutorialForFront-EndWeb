4c7e471806b9ed594ee5fc1384371f695c70b5f5
HUANG SIZHE <hsz1273327@gmail.com>
2024-05-17 15:58:55 +0800

    update

---
+++ src/wrapper/hello.android.ts
@@ -0,0 +1,14 @@
+declare const my: any
+
+export class MyHelloKotlin {
+    private helloKotlin: any
+    constructor() {
+        this.helloKotlin = new my.example.HelloKotlin()
+    }
+    get hello() {
+        return this.helloKotlin.hello
+    }
+    add(x: number, y: number) {
+        return this.helloKotlin.add(x,y)
+    }
+}
---
+++ src/wrapper/jsonschemafriend.android.ts
@@ -0,0 +1,23 @@
+declare const net: any
+
+export class JSONSchemaValidator {
+    private schemaStore: any
+    private schema: any
+    private validator: any
+    constructor(schemaString: string) {
+        this.schemaStore = new net.jimblackler.jsonschemafriend.SchemaStore()
+        this.schema = this.schemaStore.loadSchemaJson(schemaString)
+        this.validator = new net.jimblackler.jsonschemafriend.Validator()
+    }
+
+    validateJson(x: string): boolean {
+        try {
+            this.validator.validateJson(this.schema, x)
+            return true
+        } catch (error) {
+            console.log(`validator get error ${error}`)
+            return false
+        }
+
+    }
+}

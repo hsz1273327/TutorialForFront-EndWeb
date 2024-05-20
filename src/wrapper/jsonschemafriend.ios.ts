1df648c931164660a6928438b89fea103b81d550
HUANG SIZHE <hsz1273327@gmail.com>
2024-05-20 19:07:13 +0800

    Update jsonschemafriend.ios.ts

--- src/wrapper/jsonschemafriend.ios.ts
+++ src/wrapper/jsonschemafriend.ios.ts
@@ -1,38 +1,7 @@
-// declare const JSONSchema: any
-
-
-
-
-// @NativeClass()
-// class JSObject extends NSObject implements NSCoding {
-//   public encodeWithCoder(aCoder) {
-//     /* ... */
-//   }
-
-//   public initWithCoder(aDecoder) {
-//     /* ... */
-//   }
-
-//   public 'selectorWithX:andY:'(x, y) {
-//     /* ... */
-//   }
-
-//   // An array of protocols to be implemented by the native class
-//   public static ObjCProtocols = [NSCoding]
-
-//   // A selector will be exposed so it can be called from native.
-//   public static ObjCExposedMethods = {
-//     'selectorWithX:andY:': {
-//       returns: interop.types.void,
-//       params: [interop.types.id, interop.types.id],
-//     },
-//   }
-// }
-
 export class JSONSchemaValidator {
   private schema: DSJSONSchema
   constructor(schemaString: string) {
-    const content: NSString = NSString.stringWithString(schemaString)
+    const content = NSString.stringWithString(schemaString)
     console.log("content#1: " + content)
     const nsData: NSData = content.dataUsingEncoding(NSUTF8StringEncoding)
     const schemaJSON = NSJSONSerialization.JSONObjectWithDataOptionsError(nsData, null)

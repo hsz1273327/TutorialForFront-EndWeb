52e5a2f0cd0a3bacda0ad5642b97393620c4d53b
HUANG SIZHE <hsz1273327@gmail.com>
2024-05-21 16:05:43 +0800

    update

--- App_Resources/Android/src/main/java/my/example/HelloKotlin.kt
+++
@@ -1,6 +0,0 @@
-package my.example
-
-class HelloKotlin {
-  val hello = "Hello from Kotlin!"
-  fun add(a: Double, b: Double):Double { return a + b } 
-}
--- types/references.d.ts
+++ types/references.d.ts
@@ -1,3 +1,1 @@
 /// <reference path="../node_modules/@nativescript/types/index.d.ts" />
-/// <reference path="../typings/ios/x86_64/objc!nsswiftsupport.d.ts" />
-/// <reference path="../typings/ios/x86_64/objc!DSJSONSchemaValidation.d.ts" />

cc906d23431bf61cf5b66d99d261fef46bdba935
HUANG SIZHE <hsz1273327@gmail.com>
2024-07-17 15:46:00 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,15 @@
-import { createApp, registerElement } from 'nativescript-vue';
-import { themer } from '@nativescript-community/ui-material-core';
-import {  Label as HTMLLabel } from '@nativescript-community/ui-label';
-import CanvasLabel from '@nativescript-community/ui-canvaslabel/vue';
-import ImageModulePlugin from '@nativescript-community/ui-image/vue';
-import { initialize as imageInitialize} from '@nativescript-community/ui-image';
-import CanvasSVG from '@nativescript-community/ui-svg/vue';
-import { Gif } from 'nativescript-gif';
-import { BlurView } from '@nativescript-community/ui-blurview';
-import { PDFView } from '@nativescript/pdf'
+import { createApp, registerElement } from 'nativescript-vue'
+import { themer } from '@nativescript-community/ui-material-core'
+import { ColorWheel } from '@sergeymell/nativescript-color-wheel'
+import Home from './views/Home.vue'
 
-// import { MarkdownView } from "nativescript-markdown-view"
-
-imageInitialize({isDownsampleEnabled: true});
-
-registerElement('HTMLLabel', () => HTMLLabel);
-registerElement('Gif', () => Gif);
-registerElement('BlurView', () => BlurView);
-registerElement("AutoFitText", ()=>require("@nativescript/auto-fit-text").AutoFitText)
-registerElement('PDFView', () => PDFView);
-// registerElement('MarkdownView', () =>MarkdownView)
-// import Home from './views/Page_show_label.vue'
-// import Home from './views/Page_show_image.vue'
-// import Home from './views/Page_show_gif.vue'
-// import Home from './views/Page_show_svg.vue'
-import Home from './views/Page_show_pdf.vue'
-// import Home from './views/Page_show_markdown.vue'
 
 if (global.isIOS) {
-    themer.setPrimaryColor('#bff937');
-    themer.setAccentColor('#ff8a39');
-    themer.setSecondaryColor('#a830d7');
-    // enableIOSDTCoreText()
+    themer.setPrimaryColor('#bff937')
+    themer.setAccentColor('#ff8a39')
+    themer.setSecondaryColor('#a830d7')
 }
 
-createApp(Home).use(CanvasLabel).use(ImageModulePlugin).use(CanvasSVG).start();
 
-// createApp(Home).use(ImageModulePlugin).use(CanvasSVG).start();
+registerElement('ColorWheel', () => ColorWheel)
+createApp(Home).start()
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <Frame>
+        <Page actionBarHidden="true">
+            <StackLayout>
+                <Label :text="current_color_code" :backgroundColor="current_color" />
+                <ColorWheel width="200" height="200" margin="20" @colorSelect="selectColor" />
+            </StackLayout>
+        </Page>
+    </Frame>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'nativescript-vue'
+import { EventData } from '@nativescript/core'
+import { ColorWheel } from '@sergeymell/nativescript-color-wheel'
+
+const current_color_code = ref("请选择颜色")
+const current_color = ref("white")
+
+function selectColor(evt: EventData) {
+    let obj = evt.object as ColorWheel
+    current_color_code.value = obj.color.hex
+    current_color.value = obj.color.hex
+}
+</script>

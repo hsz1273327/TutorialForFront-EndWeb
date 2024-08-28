26e65766fff371f27c8d2866f2ae5f98d47bfbc9
HUANG SIZHE <hsz1273327@gmail.com>
2024-08-28 15:00:55 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { MyVuePlugin } from "myvueplugin"
 import TestComponents from 'myvueplugin/components/TestComponents.vue'
 
 const app = createApp(App)
-// app.use(MyVuePlugin, { withComponents: true })
-app.use(MyVuePlugin, { "TestComponents": TestComponents })
+// app.use(MyVuePlugin, { withAllComponents: true })
+app.use(MyVuePlugin, { TestComponents })
 app.mount('#app')

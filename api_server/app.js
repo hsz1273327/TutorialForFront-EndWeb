109f2cac4677415bd03554587ba67f57207add2a
HUANG SIZHE <hsz1273327@gmail.com>
2024-09-02 11:05:33 +0800

    Update app.js

--- api_server/app.js
+++ api_server/app.js
@@ -5,6 +5,7 @@ import cors from 'koa2-cors'
 import { apiRouter } from './controller/index.js'
 const app = new Koa()
 
+// app.use(cors())
 app.use(cors({
     origin: "http://localhost:8080",
     credentials: true

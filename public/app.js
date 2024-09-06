7a348735669a0b7ee225981c35f6311ba12c7e7e
HUANG SIZHE <hsz1273327@gmail.com>
2024-09-06 18:55:04 +0800

    update

--- public/app.js
+++ public/app.js
@@ -1,21 +1,43 @@
 import { Gallery } from './image-list.js';
 
 const registerServiceWorker = async () => {
+  // 判断浏览器支不支持serviceWorker特性
   if ('serviceWorker' in navigator) {
     try {
+      // 先强制取消注册
+      const registrations = await navigator.serviceWorker.getRegistrations()
+      for (let r of registrations){
+        if (r.scope.includes("read")){
+          const ok = await r.unregister()
+          console.log(`Service worker ${r.scope} unregisted -- ${ok}`);
+        }
+      }
+      // 注册serviceworker
+      // 这个接口会向服务端请求`sw.js`
       const registration = await navigator.serviceWorker.register(
         'sw.js',
         {
-          scope: './',
+          scope: '/',
         }
-      );
+      )
       if (registration.installing) {
+        // registration.installing不为null则说明Service worker正在安装
         console.log('Service worker installing');
       } else if (registration.waiting) {
+        // registration.waiting不为null则说明Service worker已经安装好
         console.log('Service worker installed');
       } else if (registration.active) {
+        // registration.active不为null则说明Service worker已经激活
         console.log('Service worker active');
       }
+      navigator.serviceWorker.addEventListener("message", (event) => {
+        console.log(`Get message from worker ${event.data}`);
+      });
+      navigator.serviceWorker.startMessages()
+      await navigator.serviceWorker.ready
+      registration.active.postMessage(
+        "Test message sent immediately after creation from main to worker",
+      )
     } catch (error) {
       console.error(`Registration failed with ${error}`);
     }
--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,31 @@
+const SW_VERSION = "v1"
+const SW_CACHE_RESOURCE = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './image-list.js',
+  './star-wars-logo.jpg',
+  './gallery/bountyHunters.jpg',
+  './gallery/myLittleVader.jpg',
+  './gallery/snowTroopers.jpg',
+]
+
 const addResourcesToCache = async (resources) => {
-  const cache = await caches.open('v1');
-  await cache.addAll(resources);
-};
+  const cache = await caches.open(SW_VERSION)
+  await cache.addAll(resources)
+}
 
 const putInCache = async (request, response) => {
-  const cache = await caches.open('v1');
-  await cache.put(request, response);
-};
+  const cache = await caches.open('v1')
+  await cache.put(request, response)
+}
 
 const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
   // First try to get the resource from the cache
-  const responseFromCache = await caches.match(request);
+  const responseFromCache = await caches.match(request)
   if (responseFromCache) {
-    return responseFromCache;
+    return responseFromCache
   }
 
   // Next try to use the preloaded response, if it's there
@@ -21,25 +34,25 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
   // https://github.com/mdn/dom-examples/issues/145
   // To avoid those errors, remove or comment out this block of preloadResponse
   // code along with enableNavigationPreload() and the "activate" listener.
-  const preloadResponse = await preloadResponsePromise;
+  const preloadResponse = await preloadResponsePromise
   if (preloadResponse) {
-    console.info('using preload response', preloadResponse);
-    putInCache(request, preloadResponse.clone());
-    return preloadResponse;
+    console.info('using preload response', preloadResponse)
+    putInCache(request, preloadResponse.clone())
+    return preloadResponse
   }
 
   // Next try to get the resource from the network
   try {
-    const responseFromNetwork = await fetch(request.clone());
+    const responseFromNetwork = await fetch(request.clone())
     // response may be used only once
     // we need to save clone to put one copy in cache
     // and serve second one
-    putInCache(request, responseFromNetwork.clone());
-    return responseFromNetwork;
+    putInCache(request, responseFromNetwork.clone())
+    return responseFromNetwork
   } catch (error) {
-    const fallbackResponse = await caches.match(fallbackUrl);
+    const fallbackResponse = await caches.match(fallbackUrl)
     if (fallbackResponse) {
-      return fallbackResponse;
+      return fallbackResponse
     }
     // when even the fallback response is not available,
     // there is nothing we can do, but we must always
@@ -47,36 +60,27 @@ const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
     return new Response('Network error happened', {
       status: 408,
       headers: { 'Content-Type': 'text/plain' },
-    });
+    })
   }
-};
+}
 
 const enableNavigationPreload = async () => {
   if (self.registration.navigationPreload) {
     // Enable navigation preloads!
-    await self.registration.navigationPreload.enable();
+    await self.registration.navigationPreload.enable()
   }
-};
+}
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(enableNavigationPreload());
-});
+  event.waitUntil(enableNavigationPreload())
+})
 
 self.addEventListener('install', (event) => {
+  // self.skipWaiting()
   event.waitUntil(
-    addResourcesToCache([
-      './',
-      './index.html',
-      './style.css',
-      './app.js',
-      './image-list.js',
-      './star-wars-logo.jpg',
-      './gallery/bountyHunters.jpg',
-      './gallery/myLittleVader.jpg',
-      './gallery/snowTroopers.jpg',
-    ])
-  );
-});
+    addResourcesToCache(SW_CACHE_RESOURCE)
+  )
+})
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
@@ -85,5 +89,33 @@ self.addEventListener('fetch', (event) => {
       preloadResponsePromise: event.preloadResponse,
       fallbackUrl: './gallery/myLittleVader.jpg',
     })
-  );
-});
+  )
+})
+
+
+addEventListener("message", (event) => {
+  event.waitUntil(
+    (async () => {
+      console.log(`Message received: ${ event.data }`)
+      // // Get the client.
+      // if (!event.clientId) {
+      //   console.log(`event.clientId not found`)
+      //   return
+      // }
+      // const client = await self.clients.get(event.clientId)
+      // // Exit early if we don't get the client.
+      // // Eg, if it closed.
+      // if (!client) {
+      //   console.log(`client not found`)
+      //   return
+      // }
+      // client.postMessage("This is a message from sw")
+      const clients = await self.clients.matchAll()
+      clients.forEach(function(client) {
+        if (client.url.includes("/my-account")) {
+          client.postMessage("Hi client: "+client.id);
+      } });
+      event.source.postMessage("This is a message from sw")
+    })()
+  )
+})

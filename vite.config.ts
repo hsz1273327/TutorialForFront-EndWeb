39abfa68b1c90912b74fc471fd3a3c219cfaf160
HUANG SIZHE <hsz1273327@gmail.com>
2024-09-13 19:35:31 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { VitePWA } from 'vite-plugin-pwa';
 
 
 // https://vitejs.dev/config/
@@ -21,6 +22,7 @@ export default defineConfig({
         ElementPlusResolver(),
       ],
     }),
+    VitePWA({ registerType: 'autoUpdate' }),
   ],
   resolve: {
     alias: {

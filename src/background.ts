fc8ab840796c85beefa66be96a5f19df7d6da508
HUANG SIZHE <hsz1273327@gmail.com>
2024-10-14 19:30:47 +0800

    update

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,14 @@
-function gotoSinaFinance(symbol: string) {
-    console.log(`get symbol ${symbol}`)
-    window.open(`https://finance.sina.com.cn/realstock/company/${symbol}/nc.shtml`)
+async function gotoSinaFinance(symbol: string): Promise<void> {
+    await chrome.tabs.create({ url: `https://finance.sina.com.cn/realstock/company/${symbol}/nc.shtml` })
+    const historySymbolsInStorage = await chrome.storage.local.get("history-symbols")
+    let historySymbolsSet = new Set(historySymbolsInStorage["history-symbols"])
+    historySymbolsSet.add(symbol)
+    const historySymbols = Array.from(historySymbolsSet)
+    await chrome.storage.local.set({
+        "history-symbols": historySymbols
+    })
+    const historySymbolsCount = historySymbols.length
+    await chrome.action.setBadgeText({ text: `${historySymbolsCount}` })
 }
 
 chrome.omnibox.setDefaultSuggestion(

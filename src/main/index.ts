256bbff04f9c930200253e35f376dc841a148967
HUANG SIZHE <hsz1273327@gmail.com>
2025-03-25 19:33:21 +0800

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,94 @@
-import { app, shell, BrowserWindow, ipcMain, Notification, Menu, MenuItem } from 'electron'
+import {
+  app,
+  shell,
+  BrowserWindow,
+  ipcMain,
+  Notification,
+  Tray,
+  nativeImage,
+  Menu,
+  MenuItem
+} from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
-function createWindow(): void {
+// 系统托盘设置
+function init_tray(): void {
+  const tray_icon = nativeImage.createFromPath(icon)
+  const tray = new Tray(tray_icon)
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio' },
+    { label: '退出', type: 'normal', click: (): void => app.quit() }
+  ])
+  tray.setContextMenu(contextMenu)
+  //设置鼠标指针在托盘图标上悬停时显示的文本(linux下不支持)
+  tray.setToolTip('This is my application')
+  // 设置macos中显示在状态栏中托盘图标旁边的标题
+  tray.setTitle('This is my title')
+}
+
+// 增加菜单功能
+function init_application_menu(mainWindow: BrowserWindow): void {
+  const menu = new Menu()
+  menu.append(
+    new MenuItem({
+      label: '关闭到后台',
+      submenu: [
+        {
+          role: 'help',
+          accelerator: process.platform === 'darwin' ? 'Alt+Cmd+I' : 'Alt+Shift+I',
+          click: (): void => {
+            console.log('Electron rocks!')
+            new Notification({
+              title: 'test electron app message',
+              body: `submenu help clicked`
+            }).show()
+          }
+        }
+      ]
+    })
+  )
+  menu.append(
+    new MenuItem({
+      label: 'Electron-1',
+      submenu: [
+        {
+          role: 'quit',
+          accelerator: process.platform === 'darwin' ? 'Alt+Cmd+I' : 'Alt+Shift+I',
+          click: (): void => {
+            console.log('Electron rocks!')
+            new Notification({
+              title: 'test electron app message',
+              body: `submenu help clicked`
+            }).show()
+          }
+        }
+      ]
+    })
+  )
+  Menu.setApplicationMenu(menu)
+  // 确保显示菜单
+  mainWindow.setMenuBarVisibility(true)
+  mainWindow.setAutoHideMenuBar(false)
+}
+// 设置macos下dock的行为
+function init_dock(): void {
+  app.dock.setIcon(icon)
+  app.dock.setBadge('my application')
+  app.dock.setMenu(
+    Menu.buildFromTemplate([
+      { label: 'New Window', click: (): void => console.log('New Window') },
+      { label: 'New Window with Settings', submenu: [{ label: 'Basic' }, { label: 'Pro' }] },
+      { label: 'New Command...' }
+    ])
+  )
+}
+
+function createWindow(): BrowserWindow {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 900,
@@ -16,9 +101,7 @@ function createWindow(): void {
       sandbox: false
     }
   })
-  // 确保显示菜单
-  mainWindow.setMenuBarVisibility(true)
-  mainWindow.setAutoHideMenuBar(false)
+
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
   })
@@ -35,47 +118,8 @@ function createWindow(): void {
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
+  return mainWindow
 }
-
-// 增加菜单功能
-
-const menu = new Menu()
-menu.append(
-  new MenuItem({
-    label: 'Electron-0',
-    submenu: [
-      {
-        role: 'help',
-        accelerator: process.platform === 'darwin' ? 'Alt+Cmd+I' : 'Alt+Shift+I',
-        click: (): void => {
-          console.log('Electron rocks!')
-          new Notification({
-            title: 'test electron app message',
-            body: `submenu help clicked`
-          }).show()
-        }
-      }
-    ]
-  })
-)
-menu.append(
-  new MenuItem({
-    label: 'Electron-1',
-    submenu: [
-      {
-        role: 'quit',
-        accelerator: process.platform === 'darwin' ? 'Alt+Cmd+I' : 'Alt+Shift+I',
-        click: (): void => {
-          console.log('Electron rocks!')
-          new Notification({
-            title: 'test electron app message',
-            body: `submenu help clicked`
-          }).show()
-        }
-      }
-    ]
-  })
-)
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -83,9 +127,6 @@ menu.append(
 app.whenReady().then(() => {
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
-  Menu.setApplicationMenu(menu)
-  console.log(Menu.getApplicationMenu())
-
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
@@ -96,8 +137,12 @@ app.whenReady().then(() => {
   // IPC test
   ipcMain.on('ping', () => console.log('pong'))
 
-  createWindow()
-
+  const mainwindow = createWindow()
+  init_application_menu(mainwindow)
+  init_tray()
+  if (process.platform !== 'darwin') {
+    init_dock()
+  }
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
@@ -108,10 +153,6 @@ app.whenReady().then(() => {
 // 生命周期监听
 app.on('browser-window-blur', (_event, window) => {
   console.log(`${window.id} browser-window-blur`)
-  new Notification({
-    title: 'test electron app message',
-    body: `${window.id} browser-window-blur`
-  }).show()
 })
 app.on('browser-window-focus', (_event, window) => {
   console.log(`${window.id} browser-window-focus`)

a6535dd2598b4d0b6feca6b42b8699936a44d1d4
HUANG SIZHE <hsz1273327@gmail.com>
2025-04-03 17:59:08 +0800

    Update linux_init.ts

--- src/main/linux_init.ts
+++ src/main/linux_init.ts
@@ -11,50 +11,58 @@ const target_icon_path = join(home_path, `.local/share/icons/${appName}.png`)
 const appDir = process.env.APPDIR
 
 async function init_linux(): Promise<void> {
-  if (!existsSync(target_desktop_path)) {
-    // 不存在则让用户选择是否安装
-    const userResponse = await dialog.showMessageBox({
-      type: 'question',
-      buttons: ['是', '否'],
-      defaultId: 0,
-      title: '系统集成',
-      message: '是否将应用程序集成到系统中?(添加到应用菜单)'
-    })
-    if (userResponse.response === 0) {
-      installDesktopFile()
+  if (appDir) {
+    if (!existsSync(target_desktop_path)) {
+      // 当为appimage程序且不存在.desktop时让用户选择是否进行集成
+      const userResponse = await dialog.showMessageBox({
+        type: 'question',
+        buttons: ['是', '否'],
+        defaultId: 0,
+        title: '系统集成',
+        message: '是否将应用程序集成到系统中?(添加到应用菜单)'
+      })
+      if (userResponse.response === 0) {
+        installDesktopFile()
+      }
+    } else {
+      // 如果已经存在.desktop文件,判断其内容是否一致
+      const source_content = await getSourceDesktopFileContent()
+      const target_content = await fs.readFile(target_desktop_path, 'utf-8')
+      if (source_content !== target_content) {
+        // 如果内容不一致,提示用户是否覆盖
+        const userResponse = await dialog.showMessageBox({
+          type: 'question',
+          buttons: ['是', '否'],
+          defaultId: 0,
+          title: '系统集成',
+          message: '是否更新应用程序的集成集成配置?(更新应用菜单)'
+        })
+        if (userResponse.response === 0) {
+          installDesktopFile()
+        }
+      }
     }
   }
 }
+
+async function getSourceDesktopFileContent(): Promise<string> {
+  const source_dir = appDir as string
+  const appimage_dir = process.cwd()
+  const source_desktop_path = join(source_dir, `${appName}.desktop`)
+  let result = await fs.readFile(source_desktop_path, 'utf-8')
+  // 替换Exec
+  result = result.replace(/Exec=AppRun/g, `Exec=${appimage_dir}/${appName}-${appVersion}.AppImage`)
+  // 替换Icon
+  result = result.replace(`Icon=${appName}`, `Icon=${target_icon_path}`)
+  return result
+}
+
 async function installDesktopFile(): Promise<void> {
   try {
-    let source_dir: string
-    if (appDir) {
-      source_dir = appDir
-      console.log(`appDir: ${appDir}`)
-      const appimage_dir = process.cwd()
-      console.log(`process.cwd: ${appimage_dir}`)
-      const source_desktop_path = join(source_dir, `${appName}.desktop`)
-      let result = await fs.readFile(source_desktop_path, 'utf-8')
-
-      result = result.replace(
-        /Exec=AppRun/g,
-        `Exec=${appimage_dir}/${appName}-${appVersion}.AppImage`
-      )
-      // result = result.replace(/Icon=helloworld/g, `Icon=${target_icon_path}`)
-      result = result.replace(`Icon=${appName}`, `Icon=${target_icon_path}`)
-      console.log(`.desktop: ${result}`)
-      // 复制 .desktop 文件
-      await fs.writeFile(target_desktop_path, result)
-      console.log(`.desktop 文件已部署到: ${target_desktop_path}`)
-    } else {
-      source_dir = process.cwd()
-      console.log(`process.cwd: ${source_dir}`)
-      const source_desktop_path = join(source_dir, `${appName}.desktop`)
-      // 复制 .desktop 文件
-      await fs.copyFile(source_desktop_path, target_desktop_path)
-      console.log(`.desktop 文件已复制到: ${target_desktop_path}`)
-    }
-    const source_icon_path = join(source_dir, `${appName}.png`)
+    const result = await getSourceDesktopFileContent()
+    await fs.writeFile(target_desktop_path, result)
+    console.log(`.desktop 文件已部署到: ${target_desktop_path}`)
+    const source_icon_path = join(appDir as string, `${appName}.png`)
     // 复制图标文件
     await fs.copyFile(source_icon_path, target_icon_path)
     console.log(`图标文件已复制到: ${target_icon_path}`)

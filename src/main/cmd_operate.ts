3d84e61354cdd3dade8b717ea6a9054bf2d0f79f
HUANG SIZHE <hsz1273327@gmail.com>
2025-04-22 15:59:25 +0800

    update

---
+++ src/main/cmd_operate.ts
@@ -0,0 +1,17 @@
+import { Command, Option, OptionValues } from 'commander'
+
+function getCmdOptions(): OptionValues {
+  const program = new Command()
+  program
+    .option('--can_background', '是否允许后台运行') // 默认不启用调试模式
+    .option('--window_hide_as_close', '是否将窗口的关闭改为隐藏') // 默认不启用调试模式
+    .addOption(new Option('--usercmd <char>', '用户命令').choices(['cleansetting', 'exit']))
+    .description('Electron 应用程序')
+
+  program.parse(process.argv)
+  const options = program.opts()
+  console.log('解析的参数:', options)
+  return options
+}
+
+export { getCmdOptions }
---
+++ src/main/window_operate.ts
@@ -0,0 +1,81 @@
+// 单窗口应用
+import { shell, BrowserWindow } from 'electron'
+import { join } from 'path'
+import { is } from '@electron-toolkit/utils'
+
+import icon from '../../resources/icon.png?asset'
+import { getSetting } from './setting'
+
+let mainWindow: BrowserWindow | null = null
+
+// 创建窗口
+function createWindow(): BrowserWindow {
+  // Create the browser window.
+  const Window = new BrowserWindow({
+    title: 'helloworld',
+    width: 900,
+    height: 670,
+    show: false,
+    autoHideMenuBar: true,
+    ...(process.platform === 'linux' ? { icon } : {}),
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      sandbox: false
+    }
+  })
+
+  Window.on('ready-to-show', () => {
+    Window.show()
+  })
+  // 拦截窗口关闭事件
+  Window.on('close', (event) => {
+    const all_windows = BrowserWindow.getAllWindows()
+    if (all_windows.length == 1) {
+      // 如果只有一个窗口,检查是否需要拦截
+      const setting = getSetting()
+      // 检查设置,如果设置为允许后台运行，则拦截
+      if (setting.can_background) {
+        console.log('Window close event triggered')
+        event.preventDefault() // 阻止默认关闭行为
+        if (setting.window_hide_as_close) {
+          console.log('window_hide_as_close')
+          Window.hide() // 隐藏窗口
+        } else {
+          Window?.destroy() // 销毁窗口对象
+          mainWindow = null
+          console.log('Window destroy')
+        }
+      }
+    }
+  })
+
+  Window.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url)
+    return { action: 'deny' }
+  })
+
+  // HMR for renderer base on electron-vite cli.
+  // Load the remote URL for development or the local html file for production.
+  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+    Window.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  } else {
+    Window.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+  mainWindow = Window
+  return Window
+}
+
+function showWindow(): void {
+  if (mainWindow) {
+    mainWindow.show() // 显示窗口
+    if (mainWindow.isMinimized()) {
+      mainWindow.restore() // 如果窗口最小化，恢复窗口
+    }
+    mainWindow.focus() // 将窗口置于焦点
+  } else {
+    console.log('mainWindow is null')
+    mainWindow = createWindow()
+  }
+}
+
+export { createWindow, showWindow }

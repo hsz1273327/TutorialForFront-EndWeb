7cf002ea3056a136d0cd831a8860acf3b315fc11
HUANG SIZHE <hsz1273327@gmail.com>
2025-04-23 23:22:21 +0800

    Update ipc.ts

--- src/main/ipc.ts
+++ src/main/ipc.ts
@@ -1,6 +1,12 @@
 import { app, ipcMain } from 'electron'
 
-function 
+function init_ipc() {
+  // Handle the 'get-app-path' event
+  ipcMain.handle('get-app-path', () => {
+    return app.getAppPath()
+  })
+
+  // Handle the 'set-title' event
 ipcMain.on('set-title', (event, title) => {
     const webContents = event.sender
 
